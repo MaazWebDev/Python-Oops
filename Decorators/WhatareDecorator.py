8dea5f1da025f6da3044d8fa75963b95c0c6db88
MaazWebDev <maazkhan66376@gmail.com>
2025-09-07 19:43:16 +0500

    Decorators

---
+++ Decorators/WhatareDecorator.py
@@ -0,0 +1,9 @@
+# Decorators : In Python, decorators are flexible way to modify or extend behavior of functions or methods, without changing their actual code.
+
+
+# Problem 1: Timing Function Execution
+# Problem 2: Debugging Function Calls
+# Problem 3: Cache Return Values
+
+# A decorator is essentially a function that takes another function as an argument and returns a new function with enhanced functionality.
+# Decorators are often used in scenarios such as logging, authentication and memorization, allowing us to add additional functionality to existing functions or methods in a clean, reusable way.
